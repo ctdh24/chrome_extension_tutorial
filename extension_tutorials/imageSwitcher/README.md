99cc03f04f73ecb65edc4bd6593b1efaede18f4b
calvin.huynh <calvin.huynh@objectbrains.com>
2018-04-17 11:54:28 -0700

    img switcher extension

---
+++ extension_tutorials/imageSwitcher/README.md
@@ -0,0 +1,2 @@
+#Image Switcher tutorial
+original tutorial posted on http://atsusi.me/blog/Your-First-Chrome-Extension/
---
+++ extension_tutorials/imageSwitcher/switch.js
@@ -0,0 +1,4 @@
+for (var i = 0; document.images.length; i++) {
+	//change url
+	document.images[i].src = "YOUR_IMG_URL"
+};
